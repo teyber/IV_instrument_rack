edb53aa92b61d6264b2ff030c1e7f4fa6203043f
Reed Teyber <rpteyber@gmail.com>
2021-03-25 18:36:23 -0700

    initial heater

--- RT_instrument_rack.py
+++ RT_instrument_rack.py
@@ -14,12 +14,19 @@ from measurement_routines import *
 def main():
 
 	rm = visa.ResourceManager()
+        
+	print(rm.list_resources())
+
 	
+	ramp_time = 1
+	quick_psu_ramp(rm, I_amps= 30, up_ramp_time = ramp_time, dwell_time = 30, down_ramp_time = ramp_time, setup_time = 5)
 
-	top_control_IV_curve(rm)
 
-	# ramp_time = 5
-	# quick_psu_ramp(rm, I_amps= 370, up_ramp_time = ramp_time, dwell_time = 50, down_ramp_time = ramp_time, setup_time = 10)
+	#Fire Heater
+	# pulse_duration = 0.5 # seconds
+	# init_heater()
+	# fire_heater(pulse_duration)
+
 
 	
 
@@ -31,7 +38,7 @@ def main():
 def top_control_IV_curve(rm):
 
 # #Run IV curve, measure both up and down ramps
-	test_code = 'corc_june_10_2020'
+	test_code = 'corc_Bz_jan_20210'
 
 	#Initialize instruments
 	nanovm = init_nanovm(rm, max_voltage = 0.01, NPLC = 1)
