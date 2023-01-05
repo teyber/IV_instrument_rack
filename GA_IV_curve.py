caa5780e3d4bf464e970bc830a4a28146eb34c0a
Reed Teyber <31120794+teyber@users.noreply.github.com>
2023-01-05 10:39:40 -0800

    backup with new naming

--- GA_IV_curve.py
+++ GA_IV_curve.py
@@ -63,7 +63,7 @@ def run_IV_curve(rm, SSTF_psu, I_start, I_end, I_inc, test_code):
 
 	#IV parameters
 	I_vec = np.arange(I_start, I_end + I_inc, I_inc)
-	V_sample_max = 10/1000 #Disable PSU if voltage exceeds this
+	V_sample_max = 20/1000 #Disable PSU if voltage exceeds this
 	t_settle = 0.5 #time to wait before recording voltage
 	t_plot = 0.5 #time to plot
 	t_record = 0.5 #time for NI dAQ to average
@@ -89,28 +89,24 @@ def run_IV_curve(rm, SSTF_psu, I_start, I_end, I_inc, test_code):
 	#Initialize vectors to be filled in IV curve
 	num_points = np.size(I_vec)
 	time_array = np.zeros(num_points)
+	Vs_GA1 = np.zeros(num_points)
+	Vs_GA2 = np.zeros(num_points)
+	Vs_CORE = np.zeros(num_points)
+	Vs_OUTER = np.zeros(num_points)
+	Vs_INNER = np.zeros(num_points)
 	I_shunt = np.zeros(num_points)
-	Vs_1 = np.zeros(num_points)
-	Vs_2 = np.zeros(num_points)
-	Vs_3 = np.zeros(num_points)
-	Vs_4 = np.zeros(num_points)
-	Vs_5 = np.zeros(num_points)
-	Vs_6 = np.zeros(num_points)
-	Vs_7 = np.zeros(num_points)
 
 
 	#Check starting point before telling power supply to ramp
 	time_array[0] = time.time()
-	I_shunt_init, Vs_1_init, Vs_2_init, Vs_3_init, Vs_4_init, Vs_5_init, Vs_6_init, Vs_7_init = get_cDAQ_8ch(t_record)
+	Vs_GA1_i, Vs_GA2_i, Vs_CORE_i, Vs_OUTER_i, Vs_INNER_i, I_shunt_i = get_cDAQ_8ch(t_record)
 
-	I_shunt[0] = I_shunt_init
-	Vs_1[0] = Vs_1_init
-	Vs_2[0] = Vs_2_init
-	Vs_3[0] = Vs_3_init
-	Vs_4[0] = Vs_4_init
-	Vs_5[0] = Vs_5_init
-	Vs_6[0] = Vs_6_init
-	Vs_7[0] = Vs_7_init
+	Vs_GA1[0] = Vs_GA1_i
+	Vs_GA2[0] = Vs_GA2_i
+	Vs_CORE[0] = Vs_CORE_i
+	Vs_OUTER[0] = Vs_OUTER_i
+	Vs_INNER[0] = Vs_INNER_i
+	I_shunt[0] = I_shunt_i
 
 
 	plt.close('all')
@@ -126,29 +122,28 @@ def run_IV_curve(rm, SSTF_psu, I_start, I_end, I_inc, test_code):
 		#Get voltages from meters
 		time_array[i] = time.time()
 
-		I_shunt_i, Vs_1_i, Vs_2_i, Vs_3_i, Vs_4_i, Vs_5_i, Vs_6_i, Vs_7_i = get_cDAQ_8ch(t_record)
-		I_shunt[i] = I_shunt_init
-		Vs_1[i] = Vs_1_i
-		Vs_2[i] = Vs_2_i
-		Vs_3[i] = Vs_3_i
-		Vs_4[i] = Vs_4_i
-		Vs_5[i] = Vs_5_i
-		Vs_6[i] = Vs_6_i
-		Vs_7[i] = Vs_7_i
+		Vs_GA1_i, Vs_GA2_i, Vs_CORE_i, Vs_OUTER_i, Vs_INNER_i, I_shunt_i = get_cDAQ_8ch(t_record)
+
+		Vs_GA1[i] = Vs_GA1_i
+		Vs_GA2[i] = Vs_GA2_i
+		Vs_CORE[i] = Vs_CORE_i
+		Vs_OUTER[i] = Vs_OUTER_i
+		Vs_INNER[i] = Vs_INNER_i
+		I_shunt[i] = I_shunt_i
+
 
 		print("Programed, measured current: ", I_vec[i], I_shunt[i])
-		print('Voltages: ', Vs_1_i, Vs_2_i, Vs_3_i, Vs_4_i, Vs_5_i, Vs_6_i, Vs_7_i)
+		print('Voltages: ', Vs_GA1_i, Vs_GA2_i, Vs_CORE_i, Vs_OUTER_i, Vs_INNER_i)
 
 		np.savetxt(Path(dir_name + '/time_array.txt'), time_array)	
-		np.savetxt(Path(dir_name + '/I_vec.txt'), I_vec)		
+		np.savetxt(Path(dir_name + '/I_vec.txt'), I_vec)	
+
+		np.savetxt(Path(dir_name + '/Vs_GA1.txt'), Vs_GA1)
+		np.savetxt(Path(dir_name + '/Vs_GA2.txt'), Vs_GA2)
+		np.savetxt(Path(dir_name + '/Vs_CORE.txt'), Vs_CORE)
+		np.savetxt(Path(dir_name + '/Vs_OUTER.txt'), Vs_OUTER)
+		np.savetxt(Path(dir_name + '/Vs_INNER.txt'), Vs_INNER)
 		np.savetxt(Path(dir_name + '/I_shunt.txt'), I_shunt)
-		np.savetxt(Path(dir_name + '/Vs_1.txt'), Vs_1)
-		np.savetxt(Path(dir_name + '/Vs_2.txt'), Vs_2)
-		np.savetxt(Path(dir_name + '/Vs_3.txt'), Vs_3)
-		np.savetxt(Path(dir_name + '/Vs_4.txt'), Vs_4)
-		np.savetxt(Path(dir_name + '/Vs_5.txt'), Vs_5)
-		np.savetxt(Path(dir_name + '/Vs_6.txt'), Vs_6)
-		np.savetxt(Path(dir_name + '/Vs_7.txt'), Vs_7)
 
 
 
@@ -156,13 +151,12 @@ def run_IV_curve(rm, SSTF_psu, I_start, I_end, I_inc, test_code):
 	
 		fig = plt.figure(figsize=(8,6))
 		ax = plt.subplot(1,1,1)
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_1[0:(i+1)], 'ko--', label = 'Vs_1')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_2[0:(i+1)], 'ro--', label = 'Vs_2')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_3[0:(i+1)], label = 'Vs_3')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_4[0:(i+1)], label = 'Vs_4')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_5[0:(i+1)], label = 'Vs_5')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_6[0:(i+1)], label = 'Vs_6')
-		ax.plot(I_shunt[0:(i+1)], 1000*Vs_7[0:(i+1)], label = 'Vs_7')
+		ax.plot(I_shunt[0:(i+1)], 1000*Vs_GA1[0:(i+1)], 'ko--', label = 'Vs_GA1')
+		ax.plot(I_shunt[0:(i+1)], 1000*Vs_GA2[0:(i+1)], 'ro--', label = 'Vs_GA2')
+		ax.plot(I_shunt[0:(i+1)], 1000*Vs_CORE[0:(i+1)], label = 'Vs_CORE')
+		ax.plot(I_shunt[0:(i+1)], 1000*Vs_OUTER[0:(i+1)], label = 'Vs_OUTER')
+		ax.plot(I_shunt[0:(i+1)], 1000*Vs_INNER[0:(i+1)], label = 'Vs_INNER')
+
 
 		plt.xlabel('I [A]')
 		plt.ylabel('V [mV]')
@@ -175,13 +169,11 @@ def run_IV_curve(rm, SSTF_psu, I_start, I_end, I_inc, test_code):
 
 	fig = plt.figure(figsize=(8,6))
 	ax = plt.subplot(1,1,1)
-	ax.plot(I_shunt, 1000*Vs_1, label = 'Vs_1')
-	ax.plot(I_shunt, 1000*Vs_2, label = 'Vs_2')
-	ax.plot(I_shunt, 1000*Vs_3, label = 'Vs_3')
-	ax.plot(I_shunt, 1000*Vs_4, label = 'Vs_4')
-	ax.plot(I_shunt, 1000*Vs_5, label = 'Vs_5')
-	ax.plot(I_shunt, 1000*Vs_6, label = 'Vs_6')
-	ax.plot(I_shunt, 1000*Vs_7, label = 'Vs_7')
+	ax.plot(I_shunt, 1000*Vs_GA1, label = 'Vs_GA1')
+	ax.plot(I_shunt, 1000*Vs_GA2, label = 'Vs_GA2')
+	ax.plot(I_shunt, 1000*Vs_CORE, label = 'Vs_CORE')
+	ax.plot(I_shunt, 1000*Vs_OUTER, label = 'Vs_OUTER')
+	ax.plot(I_shunt, 1000*Vs_INNER, label = 'Vs_INNER')
 	plt.xlabel('I [A]')
 	plt.ylabel('V [mV]')
 	plt.legend(frameon=False)
@@ -235,19 +227,17 @@ def get_cDAQ_8ch(time_acquire):
 		mod2_ai2 = np.asarray(value[6])
 		mod2_ai3 = np.asarray(value[7])
 
-	V_shunt = np.mean(mod1_ai0)
+
+	Vs_GA1 = np.mean(mod1_ai0)
+	Vs_GA2 = np.mean(mod1_ai1)
+	Vs_CORE = np.mean(mod1_ai2)
+	Vs_OUTER = np.mean(mod1_ai3)
+
+	Vs_INNER = np.mean(mod2_ai0)
+	V_shunt = np.mean(mod2_ai1)
 	I_shunt = V_shunt/0.00002497
 
-	Vs_1 = np.mean(mod1_ai1)
-	Vs_2 = np.mean(mod1_ai2)
-	Vs_3 = np.mean(mod1_ai3)
-
-	Vs_4 = np.mean(mod2_ai0)
-	Vs_5 = np.mean(mod2_ai1)
-	Vs_6 = np.mean(mod2_ai2)
-	Vs_7 = np.mean(mod2_ai3)
-
-	return I_shunt, Vs_1, Vs_2, Vs_3, Vs_4, Vs_5, Vs_6, Vs_7
+	return Vs_GA1, Vs_GA2, Vs_CORE, Vs_OUTER, Vs_INNER, I_shunt
 
 
 
